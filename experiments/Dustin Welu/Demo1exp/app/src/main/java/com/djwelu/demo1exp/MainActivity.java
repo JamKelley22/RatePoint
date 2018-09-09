c0056d0aa0c89d26616133b568d0684cf57e93c3
Dustin <leopardgecko37@gmail.com>
2018-09-08 23:34:21 -0500

    constructor and variables created

--- experiments/Dustin Welu/Demo1exp/app/src/main/java/com/djwelu/demo1exp/MainActivity.java
+++ experiments/Dustin Welu/Demo1exp/app/src/main/java/com/djwelu/demo1exp/MainActivity.java
@@ -2,12 +2,50 @@ package com.djwelu.demo1exp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.os.Handler;
+import android.os.SystemClock;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView timeview;
+    Button startbutton, stopbutton;
+    Handler hand;
+    long startTime, stopTime;
+    boolean stateCounting = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        timeview = (TextView) findViewById(R.id.timeview);
+        startbutton = (Button) findViewById(R.id.startbutton);
+        stopbutton = (Button) findViewById(R.id.stopbutton);
+        hand = new Handler();
+
+        startbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startTime = SystemClock.uptimeMillis();
+                stateCounting = true;
+            }
+        });
+
+        stopbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                stopTime = SystemClock.uptimeMillis();
+                stateCounting = false;
+            }
+        });
     }
+
+    public Runnable runnable = new Runnable() {
+        public void run() {
+
+        }
+    };
+
 }
