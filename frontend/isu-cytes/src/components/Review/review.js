bf638633aa4e9f1adff6a60c6d921ef995528258
Dustin <leopardgecko37@gmail.com>
2018-09-19 20:34:16 -0500

    review form created

--- frontend/isu-cytes/src/components/Review/review.js
+++ frontend/isu-cytes/src/components/Review/review.js
@@ -5,7 +5,23 @@ import './review.css'
 class Review extends React.Component {
   render () {
     return (
-      <h1>Review</h1>
+        <div id="form">
+          <form>
+            <fieldset>
+              <legend>Write A Review</legend>
+              Title
+              <br/>
+              <input type="text" name="title" maxlength="20"/>
+              <br/>
+              Body
+              <br/>
+              <input type="text" name="body" maxlength="500" size="100"/>
+              <br/>
+              <input type="submit" value="Submit Review"/>
+            </fieldset>
+          </form>
+        </div>
+
     );
   }
 }
