600066da3704a4b7606ad646409a08b5baab8941
Dustin <leopardgecko37@gmail.com>
2018-09-20 10:35:10 -0500

    submit function created and prevent default added

--- frontend/isu-cytes/src/components/Review/review.js
+++ frontend/isu-cytes/src/components/Review/review.js
@@ -3,19 +3,27 @@ import React from 'react'
 import './review.css'
 
 class Review extends React.Component {
+
+  reviewSubmit=(e)=>{
+      console.log(e);
+      e.preventDefault();
+
+  }
+
   render () {
     return (
         <div id="form">
-          <form>
+          <h2>Name of POI</h2>
+          <form onSubmit={(e)=>this.reviewSubmit(e)}>
             <fieldset>
               <legend>Write A Review</legend>
               Title
               <br/>
-              <input type="text" name="title" maxlength="20"/>
+              <input type="text" name="title" maxLength="20"/>
               <br/>
               Body
               <br/>
-              <input type="text" name="body" maxlength="500" size="100"/>
+              <input type="text" name="body" maxLength="500" size="100"/>
               <br/>
               <input type="submit" value="Submit Review"/>
             </fieldset>
