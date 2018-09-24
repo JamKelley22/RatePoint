c8942262c0050099a9b8105e16a45d917837b1b0
Dustin <leopardgecko37@gmail.com>
2018-09-24 11:32:42 -0500

    sends data to server

--- frontend/isu-cytes/src/components/Review/review.js
+++ frontend/isu-cytes/src/components/Review/review.js
@@ -4,34 +4,59 @@ import './review.css'
 
 class Review extends React.Component {
 
-  reviewSubmit=(e)=>{
-      console.log(e);
-      e.preventDefault();
+    constructor(props) {
+        super(props);
+        this.state = {
+            poi: "Insert POI here",
+        };
+    }
 
-  }
+    reviewSubmit = (e) => {
+        e.preventDefault();
+        const formdata = new FormData(e.target);
+        let data = {
+            poidata: this.state.poi,
+            titledata: formdata.get('title'),
+            bodydata: formdata.get('body')
+        }
+        if(!e.target.checkValidity()){
+            
+            return;
+        }
+        fetch('/reviewSubmit', {method: 'PUT', body: JSON.stringify(data)})
+            .catch(function(error){
+                console.log(error);
+                return;
+            });
 
-  render () {
-    return (
-        <div id="form">
-          <h2>Name of POI</h2>
-          <form onSubmit={(e)=>this.reviewSubmit(e)}>
-            <fieldset>
-              <legend>Write A Review</legend>
-              Title
-              <br/>
-              <input type="text" name="title" maxLength="20"/>
-              <br/>
-              Body
-              <br/>
-              <input type="text" name="body" maxLength="500" size="100"/>
-              <br/>
-              <input type="submit" value="Submit Review"/>
-            </fieldset>
-          </form>
-        </div>
+    }
 
-    );
-  }
+    componentWillMount(){
+
+    }
+
+    render() {
+        return (
+            <div id="form">
+                <h2>{this.state.poi}</h2>
+                <form onSubmit={(e) => this.reviewSubmit(e)} noValidate>
+                    <fieldset>
+                        <legend>Write A Review</legend>
+                        Title
+                        <br/>
+                        <input type="text" required id="title" name="title" maxLength="20"/>
+                        <br/>
+                        Body
+                        <br/>
+                        <input type="text" required id="body" name="body" maxLength="500" size="100"/>
+                        <br/>
+                        <input type="submit" value="Submit Review"/>
+                    </fieldset>
+                </form>
+            </div>
+
+        );
+    }
 }
 
 export default Review;
