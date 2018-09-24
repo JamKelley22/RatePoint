aafb7ba3019adb13b9d2a4a2fa92ec4a21b221b2
Dustin <leopardgecko37@gmail.com>
2018-09-24 12:58:13 -0500

    check validity and clear inputs after submit created

--- frontend/isu-cytes/src/components/Review/review.js
+++ frontend/isu-cytes/src/components/Review/review.js
@@ -8,47 +8,62 @@ class Review extends React.Component {
         super(props);
         this.state = {
             poi: "Insert POI here",
+            title: '',
+            rating: '',
+            body: ''
         };
     }
 
     reviewSubmit = (e) => {
         e.preventDefault();
         const formdata = new FormData(e.target);
-        let data = {
-            poidata: this.state.poi,
-            titledata: formdata.get('title'),
-            bodydata: formdata.get('body')
-        }
         if(!e.target.checkValidity()){
-            
+            alert("not valid input");
             return;
         }
-        fetch('/reviewSubmit', {method: 'PUT', body: JSON.stringify(data)})
+        let data = {
+            poi: this.state.poi,
+            title: formdata.get('title'),
+            rating: '',
+            body: formdata.get('body')
+        };
+        console.log(JSON.stringify(data));
+        fetch('/reviewSubmit', {method: 'POST', body: JSON.stringify(data)})
             .catch(function(error){
                 console.log(error);
                 return;
             });
-
+        this.setState({title:'',rating:'',body:''});
     }
 
-    componentWillMount(){
-
+    componentDidMount(){
+        
     }
+
+    TitleChange = (e) => {
+        this.setState({ title: e.target.value });
+    };
+
+    BodyChange = (e) => {
+        this.setState({ body: e.target.value });
+    };
 
     render() {
         return (
             <div id="form">
                 <h2>{this.state.poi}</h2>
-                <form onSubmit={(e) => this.reviewSubmit(e)} noValidate>
+                <form onSubmit={(e) => this.reviewSubmit(e)} noValidate autoComplete="off">
                     <fieldset>
                         <legend>Write A Review</legend>
                         Title
                         <br/>
-                        <input type="text" required id="title" name="title" maxLength="20"/>
+                        <input type="text" required id="title" name="title" value={this.state.title}
+                               onChange={this.TitleChange} maxLength="20" autoComplete="off"/>
                         <br/>
                         Body
                         <br/>
-                        <input type="text" required id="body" name="body" maxLength="500" size="100"/>
+                        <input type="text" required id="body" name="body" value={this.state.body}
+                               onChange={this.BodyChange} autoComplete="off" maxLength="500" size="100"/>
                         <br/>
                         <input type="submit" value="Submit Review"/>
                     </fieldset>
