4f1282f26d94119627e0e368d51b9adc11306407
Omair Ijaz <oijaz@iastate.edu>
2018-09-24 16:53:29 -0500

    Change method in Review Controller

--- backend/sts-workspace/backend/src/main/java/cs309/isucytes/review/ReviewController.java
+++ backend/sts-workspace/backend/src/main/java/cs309/isucytes/review/ReviewController.java
@@ -3,6 +3,8 @@ package cs309.isucytes.review;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,12 +17,19 @@ public class ReviewController {
 	public String sayHi() {
 		return "TEST";
 	}
-	
-	//post controller
+
 	@RequestMapping(method = RequestMethod.POST, path = "/reviews/new")
-	public String saveReview(Review review) {
-		reviewRepository.save(review);
-		return "New Review " + review.getTitle() + " saved";
+	public @ResponseBody String addNewReview (@RequestParam String poi, @RequestParam String rating, 
+			@RequestParam String title, @RequestParam String body) {
+		// @ResponseBody means the returned String is the response, not a view name
+		// @RequestParam means it is a parameter from the GET or POST request
+		Review r = new Review();
+		r.setPoi(poi);
+		r.setRating(rating);
+		r.setTitle(title);
+		r.setBody(body);
+		reviewRepository.save(r);
+		return "Saved";
 	}
 	 
 }
