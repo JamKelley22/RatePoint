f3091304a928fe9ccbe4dabf9a367f30730f4acf
Omair Ijaz <oijaz@iastate.edu>
2018-09-24 15:24:20 -0500

    Class name fix

--- backend/sts-workspace/backend/src/main/java/cs309/isucytes/review/ReviewController.java
+++ backend/sts-workspace/backend/src/main/java/cs309/isucytes/review/ReviewController.java
@@ -1,10 +1,15 @@
 package cs309.isucytes.review;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ReviewController {
+
+	@Autowired
+    ReviewRepository reviewRepository;
 
 	@RequestMapping("/test")
 	public String sayHi() {
@@ -14,7 +19,7 @@ public class ReviewController {
 	//post controller
 	@RequestMapping(method = RequestMethod.POST, path = "/reviews/new")
 	public String saveReview(Review review) {
-		reviewsRepository.save(review);
+		reviewRepository.save(review);
 		return "New Review " + review.getTitle() + " saved";
 	}
 	 
