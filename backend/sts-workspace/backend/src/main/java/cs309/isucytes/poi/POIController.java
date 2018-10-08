1a4d50a9ff2314925db93ecd2f553be83482f7c7
Omair Ijaz <oijaz@iastate.edu>
2018-10-08 14:27:14 -0500

    Include method to return all pois and poi by id

--- backend/sts-workspace/backend/src/main/java/cs309/isucytes/poi/POIController.java
+++ backend/sts-workspace/backend/src/main/java/cs309/isucytes/poi/POIController.java
@@ -1,15 +1,17 @@
 package cs309.isucytes.poi;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import cs309.isucytes.poi.POI;
 import cs309.isucytes.poi.POIRepository;
+
 
 @RestController
 @RequestMapping("/poi")
@@ -18,22 +20,21 @@ public class POIController {
 	@Autowired
     POIRepository POIRepository;
 	
-	//not yet complete
-	@RequestMapping(method = RequestMethod.GET, path = "/{id}")
-	String getIdByValue(@PathVariable Integer id) {
-		return "ID is " + id;
+	
+	@RequestMapping(method = RequestMethod.GET, path = "/get/{id}")
+	public Optional<POI> getIdByValue(@PathVariable("id") Integer id) {
+		return POIRepository.findById(id);
 	}
 	
 	@RequestMapping(method = RequestMethod.POST, path = "/new")
-	public @ResponseBody String addNewReview (@RequestParam String name, @RequestParam String description, 
-			@RequestParam String coordinates, @RequestParam Boolean approved) {
-		POI poiToCreate = new POI();
-		poiToCreate.setName(name);
-		poiToCreate.setDescription(description);
-		poiToCreate.setCoordinates(coordinates);
-		poiToCreate.setApproved(approved);
-		POIRepository.save(poiToCreate);
+	public @ResponseBody String addNewReview (POI poi) {
+		POIRepository.save(poi);
 		return "Saved";
+	}
+	
+	@RequestMapping(method = RequestMethod.GET, path = "/get")
+	public @ResponseBody Iterable<POI> getAllPOIs(){
+		return POIRepository.findAll();
 	}
 
 }
