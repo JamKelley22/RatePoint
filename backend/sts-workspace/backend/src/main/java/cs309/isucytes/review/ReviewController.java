4711d85c44f770a876adef12d7b8f9e6308a9143
Sam Stifter <sam.stifter@gmail.com>
2018-10-08 15:59:50 -0500

    Modify review to take a JSON

--- backend/sts-workspace/backend/src/main/java/cs309/isucytes/review/ReviewController.java
+++ backend/sts-workspace/backend/src/main/java/cs309/isucytes/review/ReviewController.java
@@ -1,6 +1,7 @@
 package cs309.isucytes.review;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,16 +15,8 @@ public class ReviewController {
     ReviewRepository reviewRepository;
 
 	@RequestMapping(method = RequestMethod.POST, path = "/reviews/new")
-	public @ResponseBody String addNewReview (@RequestParam Integer poi, @RequestParam Integer rating, 
-			@RequestParam String title, @RequestParam String body) {
-		// @ResponseBody means the returned String is the response, not a view name
-		// @RequestParam means it is a parameter from the GET or POST request
-		Review r = new Review();
-		r.setPoi(poi);
-		r.setRating(rating);
-		r.setTitle(title);
-		r.setBody(body);
-		reviewRepository.save(r);
+	public @ResponseBody String addNewReview (@RequestBody Review review) {
+		reviewRepository.save(review);
 		return "Saved";
 	}
 	
