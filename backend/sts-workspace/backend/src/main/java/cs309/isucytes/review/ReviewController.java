825475737221e6f0eaeada84750fb7cb39723cf9
Sam Stifter <sam.stifter@gmail.com>
2018-10-08 16:24:13 -0500

    Add a method to get reviews by id

--- backend/sts-workspace/backend/src/main/java/cs309/isucytes/review/ReviewController.java
+++ backend/sts-workspace/backend/src/main/java/cs309/isucytes/review/ReviewController.java
@@ -1,10 +1,12 @@
 package cs309.isucytes.review;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,6 +25,12 @@ public class ReviewController {
 	@RequestMapping(method = RequestMethod.GET, path = "/reviews/get")
 	public @ResponseBody Iterable<Review> getAllReviews(){
 		return reviewRepository.findAll();
+	}
+	
+	@ResponseBody
+	@RequestMapping(method = RequestMethod.GET, path = "reviews/get/{id}")
+	public Optional<Review> getReviewByID(@PathVariable("id") int id){
+		return reviewRepository.findById(id);
 	}
 	 
 }
