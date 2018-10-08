a8d41ee55e7b67be33ada897d2415ced0f6b0ae4
Omair Ijaz <oijaz@iastate.edu>
2018-10-08 13:05:29 -0500

    Add getter and setter methods for poi

--- backend/sts-workspace/backend/src/main/java/cs309/isucytes/poi/POI.java
+++ backend/sts-workspace/backend/src/main/java/cs309/isucytes/poi/POI.java
@@ -27,5 +27,45 @@ public class POI {
 	@Column
 	private Boolean approved;
 	
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getCoordinates() {
+		return coordinates;
+	}
+
+	public void setCoordinates(String coordinates) {
+		this.coordinates = coordinates;
+	}
+
+	public Boolean getApproved() {
+		return approved;
+	}
+
+	public void setApproved(Boolean approved) {
+		this.approved = approved;
+	}
 }
 
