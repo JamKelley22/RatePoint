accc37f16586014db01839d10c90d2e4a33a5754
JamKelley22 <jkelley@iastate.edu>
2018-10-08 19:39:15 -0500

    added sass package and index for components

--- frontend/isu-cytes/src/App.js
+++ frontend/isu-cytes/src/App.js
@@ -5,18 +5,8 @@ import {
   Switch
 } from 'react-router-dom';
 
-import Landing from './components/Landing/landing.js';
 
-import Home from './components/Home/home.js';
-import MapContainer from './components/Map/map.js';
-import Explore from './components/Explore/explore.js';
-import Friends from './components/Friends/friends.js';
-import Suggest from './components/Suggest/suggest.js';
-import Account from './components/Account/account.js';
-
-import Review from './components/Review/review.js';
-import POI from './components/POI/poi.js';
-import Error404 from './components/Error/error404.js';
+import { Landing, Home, Map, Explore, Friends, Suggest, Account, Review, POI, Error404 } from './components'
 
 import { history } from './history.js';
 
@@ -33,7 +23,7 @@ class App extends Component {
             <Route exact path={routes._LANDING} component={() => <Landing/>} />
 
             <Route exact path={routes._HOME} component={() => <Home/>} />
-            <Route exact path={routes._MAP} component={() => <MapContainer/>} />
+            <Route exact path={routes._MAP} component={() => <Map/>} />
             <Route exact path={routes._EXPLORE} component={() => <Explore/>} />
             <Route exact path={routes._FRIENDS} component={() => <Friends/>} />
             <Route exact path={routes._SUGGEST} component={() => <Suggest/>} />
---
+++ frontend/isu-cytes/src/components/index.js
@@ -0,0 +1,14 @@
+import Account from './Account/account.js'
+import Error404 from './Error/error404.js'
+import Explore from './Explore/explore.js'
+import Friends from './Friends/friends.js'
+import Home from './Home/home.js'
+import Landing from './Landing/landing.js'
+import Map from './Map/map.js'
+import Nav from './Nav/navagation.js'
+import POI from './POI/poi.js'
+import Review from './Review/review.js'
+import Suggest from './Suggest/suggest.js'
+
+
+export { Account, Error404, Explore, Friends, Home, Landing, Map, Nav, POI, Review, Suggest }
