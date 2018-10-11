2a89a3d157e06a1284d8e0e2ba753afcb6177eab
Dustin Welu <leopardgecko37@gmail.com>
2018-10-10 22:26:36 -0500

    create account and login value functions made

--- frontend/isu-cytes/src/components/CreateAccount/createAccount.js
+++ frontend/isu-cytes/src/components/CreateAccount/createAccount.js
@@ -7,9 +7,28 @@ import './createAccount.css'
 
 class CreateAccount extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: '',
+            email: ''
+        };
+    }
+
     createRequest = (e) => {
         e.preventDefault();
         const formdata = new FormData(e.target);
+        document.getElementById('pass1').value='';
+        document.getElementById('pass2').value='';
+        this.setState({username:'',email:''});
+    };
+
+    usernameChange = (e) => {
+        this.setState({ username: e.target.value });
+    };
+
+    emailChange = (e) => {
+        this.setState({ email: e.target.value });
     };
 
     render(){
@@ -23,16 +42,18 @@ class CreateAccount extends React.Component {
                             <form onSubmit={(e) => this.createRequest(e)} noValidate autoComplete="off">
                                 <br/>
                                 <b>email:</b>
-                                <input maxLength="32" autoComplete="off"/>
+                                <input maxLength="32" autoComplete="off" value={this.state.email}
+                                       onChange={this.emailChange} required/>
                                 <br/><br/>
                                 <b>username:</b>
-                                <input maxLength="20" autoComplete="off"/>
+                                <input maxLength="20" autoComplete="off" value={this.state.username}
+                                       onChange={this.usernameChange} required/>
                                 <br/><br/>
                                 <b>password:</b>
-                                <input type="password" maxLength="32" autoComplete="off"/>
+                                <input type="password" maxLength="32" autoComplete="off" id="pass1" required/>
                                 <br/><br/>
                                 <b>confirm password:</b>
-                                <input type="password" maxLength="32" autoComplete="off"/>
+                                <input type="password" maxLength="32" autoComplete="off" id="pass2" required/>
                                 <br/><br/>
                                 <input type="submit" value="Submit" id="createSubmit"/>
                             </form>
--- frontend/isu-cytes/src/components/Login/login.js
+++ frontend/isu-cytes/src/components/Login/login.js
@@ -9,13 +9,20 @@ class Login extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            username: '',
+        };
     }
 
     loginRequest = (e) => {
         e.preventDefault();
         const formdata = new FormData(e.target);
+        document.getElementById('pass').value='';
+        this.setState({username:''});
+    };
 
+    usernameChange = (e) => {
+        this.setState({ username: e.target.value });
     };
 
     render(){
@@ -28,10 +35,11 @@ class Login extends React.Component {
                         <form onSubmit={(e) => this.loginRequest(e)} noValidate autoComplete="off">
                             <br/>
                             <b>username:</b>
-                            <input maxLength="20" autoComplete="off"/>
+                            <input maxLength="20" autoComplete="off" value={this.state.username}
+                                   onChange={this.usernameChange} required/>
                             <br/><br/>
                             <b>password:</b>
-                            <input type="password" maxLength="32" autoComplete="off"/>
+                            <input type="password" maxLength="32" autoComplete="off" id="pass" required/>
                             <br/><br/>
                             <input type="submit" value="Login"/>
                         </form>
