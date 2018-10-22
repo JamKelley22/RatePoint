caa7f6c6bae75084e65a5c536cfbe2169a6b7290
Dustin Welu <leopardgecko37@gmail.com>
2018-10-22 01:58:38 -0500

    encryption added on login page

--- frontend/isu-cytes/src/components/Login/login.js
+++ frontend/isu-cytes/src/components/Login/login.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import bcrypt from 'bcryptjs';
 import Navagation from '../Nav/navagation.js'
 import { history, routes } from '../../history.js'
 
@@ -17,9 +17,13 @@ class Login extends React.Component {
 
     loginRequest = async(e) => {
         e.preventDefault();
+        bcrypt.hash(this.state.pass1, 10, (err, hash) => {
+            console.log(hash);
+            let hashedPass = hash;
+        });
         let body = JSON.stringify({
             username: this.state.username,
-            password: this.state.pass1,
+            password: hashedPass,
         });
         let response = await fetch('http://proj309-tg-03.misc.iastate.edu:8080/people/validate', {
             method: 'POST',
