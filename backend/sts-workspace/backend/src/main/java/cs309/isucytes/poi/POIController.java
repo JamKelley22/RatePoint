49037649994f1d7e8e17551ba2dcc98e72b281b0
Omair Ijaz <oijaz@iastate.edu>
2018-10-22 11:38:03 -0500

    Add JavaDoc

--- backend/sts-workspace/backend/src/main/java/cs309/isucytes/poi/POIController.java
+++ backend/sts-workspace/backend/src/main/java/cs309/isucytes/poi/POIController.java
@@ -21,12 +21,24 @@ public class POIController {
 	@Autowired
     POIRepository POIRepository;
 	
+	/**
+	 * Returns a POI, if any, given an id
+	 * 
+	 * @param id ID to search for
+	 * @return a POI that matched with the id 
+	 */
 	@ResponseBody
 	@RequestMapping(method = RequestMethod.GET, path = "/get/{id}")
 	public Optional<POI> getPOIByID(@PathVariable("id") Integer id) {
 		return POIRepository.findById(id);
 	}
 	
+	/**
+	 * Adds a POI to the db
+	 * 
+	 * @param poi New POI to be added
+	 * @return a string that is returned on success
+	 */
 	@ResponseBody
 	@RequestMapping(method = RequestMethod.POST, path = "/new")
 	public String addNewPOI (@RequestBody POI poi) {
@@ -34,12 +46,23 @@ public class POIController {
 		return "You Saved " + poi.getName();
 	}
 	
+	/**
+	 * Gets a JSON list of all POIS
+	 * 
+	 * @return a JSON list of all POIS
+	 */
 	@ResponseBody
 	@RequestMapping(method = RequestMethod.GET, path = "/get")
 	public Iterable<POI> getAllPOIs(){
 		return POIRepository.findAll();
 	}
 	
+	/**
+	 * Deletes a POI, if any, that matches id
+	 * 
+	 * @param id id of the POI to be deleted
+	 * @return a String that is returned on success
+	 */
 	@ResponseBody
 	@RequestMapping(method = RequestMethod.DELETE, path = "/delete/{id}")
 	public String deletePOI(@PathVariable("id") Integer id){
