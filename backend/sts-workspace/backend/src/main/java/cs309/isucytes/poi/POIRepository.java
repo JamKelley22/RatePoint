a078dc2272689e444c59c551dbc2d97291523262
Omair Ijaz <oijaz@iastate.edu>
2018-11-15 10:26:54 -0600

    Create a new query to find a POI given coordinates

--- backend/sts-workspace/backend/src/main/java/cs309/isucytes/poi/POIRepository.java
+++ backend/sts-workspace/backend/src/main/java/cs309/isucytes/poi/POIRepository.java
@@ -1,5 +1,7 @@
 package cs309.isucytes.poi;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,13 @@ import org.springframework.stereotype.Repository;
 public interface POIRepository extends JpaRepository<POI, Integer> {
 	// Only need to define methods here if we need specific 
 	// queries. Fine to leave blank for now
+	
+	/**
+	 * This query will return a POI given coordinates
+	 * @param coordinates
+	 * 	The coordinates to search for
+	 * @return
+	 * 	Optional POI object
+	 */
+	public Optional<POI> findByCoordinates(String coordinates);
 }
