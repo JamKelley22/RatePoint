4b921a3d9bd87ddb3de7848e1979d284e1406ec3
jkelley <jkelley@iastate.edu>
2018-11-20 06:17:50 +0000

    Update backend/sts-workspace/backend/src/main/java/cs309/isucytes/BackendApplication.java Deleted backend/sts-workspace/backend/src/main/java/cs309/isucytes/SimpleCORSFilter.java

--- backend/sts-workspace/backend/src/main/java/cs309/isucytes/BackendApplication.java
+++ backend/sts-workspace/backend/src/main/java/cs309/isucytes/BackendApplication.java
@@ -9,4 +9,14 @@ public class BackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
+
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/lists").allowedOrigins("http://localhost:3000");
+            }
+        };
+    }
 }
