8c428863d161c8580a9f181b1cb52a76d691e107
jkelley <jkelley@iastate.edu>
2018-11-20 04:54:26 +0000

    New CrossOrigin Annotiation
    https://gist.github.com/wildoctopus/3730b5c60f9d5224f6c2418d07708e21

--- backend/sts-workspace/backend/src/main/java/cs309/isucytes/userlist/UserlistController.java
+++ backend/sts-workspace/backend/src/main/java/cs309/isucytes/userlist/UserlistController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins= {"*"}, maxAge = 4800, allowCredentials = "false" )
 @RequestMapping(path = "/lists")
 public class UserlistController {
 
@@ -28,7 +28,6 @@ public class UserlistController {
 	 * @return List if found, 404 if not
 	 */
 	@ResponseBody
-	@CrossOrigin(origins = "http://localhost:3000")
 	@RequestMapping(method = RequestMethod.GET, path = "/{id}")
 	public ResponseEntity<Userlist> getUserlistByID(@PathVariable("id") Integer id) {
 		Optional<Userlist> getList = userlistRepository.findById(id);
@@ -47,7 +46,6 @@ public class UserlistController {
 	 * @return Userlist if updated, 404 if not found
 	 */
 	@ResponseBody
-	@CrossOrigin(origins = "http://localhost:3000")
 	@RequestMapping(method = RequestMethod.PUT, path = "/{id}")
 	public ResponseEntity<Userlist> updateUserlist(@PathVariable("id") Integer id, @RequestBody Userlist userlist) {
 		Optional<Userlist> getList = userlistRepository.findById(id);
@@ -66,7 +64,6 @@ public class UserlistController {
 	 * @return List that was deleted, or 404 if it didn't exist.
 	 */
 	@ResponseBody
-	@CrossOrigin(origins = "http://localhost:3000")
 	@RequestMapping(method = RequestMethod.DELETE, path = "/{id}")
 	public ResponseEntity<Userlist> deleteUserlist(@PathVariable("id") Integer id) {
 		Optional<Userlist> getList = userlistRepository.findById(id);
