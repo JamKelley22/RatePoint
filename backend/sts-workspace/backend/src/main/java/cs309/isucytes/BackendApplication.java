f1698efc3e80d04e00fb9d1203e471407064f0ba
jkelley <jkelley@iastate.edu>
2018-11-20 06:19:33 +0000

    Update backend/sts-workspace/backend/src/main/java/cs309/isucytes/BackendApplication.java

--- backend/sts-workspace/backend/src/main/java/cs309/isucytes/BackendApplication.java
+++ backend/sts-workspace/backend/src/main/java/cs309/isucytes/BackendApplication.java
@@ -3,6 +3,11 @@ package cs309.isucytes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
 @SpringBootApplication
 public class BackendApplication {
 
