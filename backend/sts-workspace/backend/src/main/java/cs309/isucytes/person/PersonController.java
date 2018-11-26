1d4daa5f568f173455dccf257978a1943230aa96
Sam Stifter <sam.stifter@gmail.com>
2018-11-26 16:34:35 -0600

    remove controller CrossOrigin annotations

--- backend/sts-workspace/backend/src/main/java/cs309/isucytes/person/PersonController.java
+++ backend/sts-workspace/backend/src/main/java/cs309/isucytes/person/PersonController.java
@@ -6,7 +6,6 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,7 +16,6 @@ import org.springframework.web.bind.annotation.RestController;
 import cs309.isucytes.userlist.Userlist;
 
 @RestController
-@CrossOrigin
 public class PersonController {
 
 	@Autowired
--- backend/sts-workspace/backend/src/main/java/cs309/isucytes/poi/POIController.java
+++ backend/sts-workspace/backend/src/main/java/cs309/isucytes/poi/POIController.java
@@ -6,7 +6,6 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,7 +18,6 @@ import cs309.isucytes.poi.POIRepository;
 import cs309.isucytes.review.ReviewRepository;
 
 
-@CrossOrigin
 @RestController
 @RequestMapping(path = "/pois")
 public class POIController {
