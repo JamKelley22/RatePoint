eb03ed3549b60ab14d654062d5fbbee606aa82dc
Dustin Welu <leopardgecko37@gmail.com>
2018-11-27 11:10:00 -0600

    review page design changes

--- frontend/isu-cytes/src/components/Review/review.js
+++ frontend/isu-cytes/src/components/Review/review.js
@@ -89,50 +89,52 @@ class Review extends React.Component {
               <h2>{this.props.poi.name}</h2>
               <form onSubmit={(e) => this.reviewSubmit(e)} noValidate autoComplete="off">
                   <fieldset>
-                      <legend>Write A Review</legend>
-                      Title
-                      <br/>
-                      <input type="text" required id="title" name="title" value={this.state.title}
-                             onChange={this.TitleChange} maxLength="20" autoComplete="off"/>
-                      <br/>
-                      Rating
-                      <br/>
-                      <div id="container">
-                          <div id="gettable">
-                              <div id="icon1div" onMouseOver={(e) => this.RatingDisplayedChange(1)}
-                                   onClick={(e) => this.RatingChange(1)} onMouseLeave={this.RatingBackToState}
-                                    className={(this.state.display<1) ? "review__kiwi":"review__kiwi--colored"}>
-                                  <i id="icon1" className="fas fa-kiwi-bird"></i>
-                              </div>
-                              <div id="icon2div" onMouseOver={(e) => this.RatingDisplayedChange(2)}
-                                   onClick={(e) => this.RatingChange(2)} onMouseLeave={this.RatingBackToState}
-                                   className={(this.state.display<2) ? "review__kiwi":"review__kiwi--colored"}>
-                                  <i id="icon2" className="fas fa-kiwi-bird"></i>
-                              </div>
-                              <div id="icon3div" onMouseOver={(e) => this.RatingDisplayedChange(3)}
-                                   onClick={(e) => this.RatingChange(3)} onMouseLeave={this.RatingBackToState}
-                                   className={(this.state.display<3) ? "review__kiwi":"review__kiwi--colored"}>
-                                  <i id="icon3" className="fas fa-kiwi-bird"></i>
-                              </div>
-                              <div id="icon4div" onMouseOver={(e) => this.RatingDisplayedChange(4)}
-                                   onClick={(e) => this.RatingChange(4)} onMouseLeave={this.RatingBackToState}
-                                   className={(this.state.display<4) ? "review__kiwi":"review__kiwi--colored"}>
-                                  <i id="icon4" className="fas fa-kiwi-bird"></i>
-                              </div>
-                              <div id="icon5div" onMouseOver={(e) => this.RatingDisplayedChange(5)}
-                                   onClick={(e) => this.RatingChange(5)} onMouseLeave={this.RatingBackToState}
-                                   className={(this.state.display<5) ? "review__kiwi":"review__kiwi--colored"}>
-                                  <i id="icon5" className="fas fa-kiwi-bird"></i>
+                      <div id="formReview">
+                          <legend>Write A Review</legend>
+                          Title
+                          <br/>
+                          <input type="text" required id="title" name="title" value={this.state.title}
+                                 onChange={this.TitleChange} maxLength="20" autoComplete="off"/>
+                          <br/>
+                          Rating
+                          <br/>
+                          <div id="container">
+                              <div id="gettable">
+                                  <div id="icon1div" onMouseOver={(e) => this.RatingDisplayedChange(1)}
+                                       onClick={(e) => this.RatingChange(1)} onMouseLeave={this.RatingBackToState}
+                                        className={(this.state.display<1) ? "review__kiwi":"review__kiwi--colored"}>
+                                      <i id="icon1" className="fas fa-kiwi-bird"></i>
+                                  </div>
+                                  <div id="icon2div" onMouseOver={(e) => this.RatingDisplayedChange(2)}
+                                       onClick={(e) => this.RatingChange(2)} onMouseLeave={this.RatingBackToState}
+                                       className={(this.state.display<2) ? "review__kiwi":"review__kiwi--colored"}>
+                                      <i id="icon2" className="fas fa-kiwi-bird"></i>
+                                  </div>
+                                  <div id="icon3div" onMouseOver={(e) => this.RatingDisplayedChange(3)}
+                                       onClick={(e) => this.RatingChange(3)} onMouseLeave={this.RatingBackToState}
+                                       className={(this.state.display<3) ? "review__kiwi":"review__kiwi--colored"}>
+                                      <i id="icon3" className="fas fa-kiwi-bird"></i>
+                                  </div>
+                                  <div id="icon4div" onMouseOver={(e) => this.RatingDisplayedChange(4)}
+                                       onClick={(e) => this.RatingChange(4)} onMouseLeave={this.RatingBackToState}
+                                       className={(this.state.display<4) ? "review__kiwi":"review__kiwi--colored"}>
+                                      <i id="icon4" className="fas fa-kiwi-bird"></i>
+                                  </div>
+                                  <div id="icon5div" onMouseOver={(e) => this.RatingDisplayedChange(5)}
+                                       onClick={(e) => this.RatingChange(5)} onMouseLeave={this.RatingBackToState}
+                                       className={(this.state.display<5) ? "review__kiwi":"review__kiwi--colored"}>
+                                      <i id="icon5" className="fas fa-kiwi-bird"></i>
+                                  </div>
                               </div>
                           </div>
+                          <br/>
+                          Body
+                          <br/>
+                          <textarea type="text" required id="body" name="body" value={this.state.body} cols="100"
+                                    rows="5" onChange={this.BodyChange} autoComplete="off" maxLength="500"/>
+                          <br/><br/>
+                          <input type="submit" value="Submit Review"/>
                       </div>
-                      <br/>
-                      Body
-                      <br/>
-                      <textarea type="text" required id="body" name="body" value={this.state.body} cols="100"
-                                rows="5" onChange={this.BodyChange} autoComplete="off" maxLength="500"/>
-                      <br/><br/>
-                      <input type="submit" value="Submit Review"/>
                   </fieldset>
               </form>
           </div>
