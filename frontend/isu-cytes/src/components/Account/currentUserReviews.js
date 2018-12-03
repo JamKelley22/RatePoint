54a0e48f1c64b9d3c3d6ed8fbbb36131c999f353
JamKelley22 <jkelley@iastate.edu>
2018-12-02 21:44:28 -0600

    Only logged in user reviews showing in account tab

--- frontend/isu-cytes/src/components/Account/currentUserReviews.js
+++ frontend/isu-cytes/src/components/Account/currentUserReviews.js
@@ -1,23 +1,32 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import Rating from '../POI/rating.js'
+
 import './account.scss'
 
 const CurrentUserReviews = (props) => {
   return (
     <div className='tabContent currentUserReviewsComponent'>
       {
-        props.reviews > 0
+        props.reviews.length > 0
         ?
-        props.reviews.map((review ,i) =>
-          <div className='reviewItem' key={i}>
-            {review.title}
-            Rating: {review.rating}
-          </div>
-        )
+        props.reviews.map((review ,i) => {
+          console.log(review);
+          return (
+            <div className='reviewItem' key={i}>
+              <strong>{props.getPOIName(review.poi)}<br/></strong>
+              {review.title}
+              <Rating
+                number={review.rating}
+              />
+              <a onClick={() => props.viewPOIByNum(review.poi)}>View POI</a>
+            </div>
+          )
+        })
         :
         //=====
-        <h3>No Reviews</h3>
+        <h3>No Reviews Yet!</h3>
         //=====
       }
     </div>
